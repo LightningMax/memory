b86a6ccdc6323953729ceeda3db52361242650a7
Max Reis <maxreis507@gmail.com>
2025-12-31 13:27:20 +0100

    feat: add Board and Card components

---
+++ src/components/Board.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Card from "./Card.vue";
+import { cards, flipCard } from "@/composables/useGameLogic";
+</script>
+
+<template>
+  <pre>{{ cards }}</pre>
+  <div class="cards">
+    <Card
+      v-for="card in cards"
+      :key="card.id"
+      :card="card"
+      @select="flipCard"
+    />
+  </div>
+</template>
+
+<style>
+.cards {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  width: 400px;
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,30 @@
+<script setup>
+const props = defineProps({
+  card: {
+    type: Object,
+    required: true,
+  },
+});
+const emit = defineEmits(["select"]);
+
+function handleClick() {
+  emit("select", props.card);
+}
+</script>
+
+<template>
+  <div class="card" @click="handleClick">
+    {{ props.card.isFlipped ? props.card.value : "?" }}
+  </div>
+</template>
+
+<style scoped>
+.card {
+  cursor: pointer;
+  padding: 10px;
+  margin: 5px;
+  border: 1px solid black;
+  width: 50px;
+  text-align: center;
+}
+</style>
